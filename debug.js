c1d848107c2317be5e19ebd33ed799b0fd9010f1
Jeroen <jeroenooms@gmail.com>
2013-08-04 11:54:30 +0200

    update default val

--- debug.js
+++ debug.js
@@ -178,17 +178,10 @@ $(document).ready(
 		function() {
 			// something for open mHealth
 			if (document.location.hostname.slice(-6) == "omh.io") {
-				$("#urlvalue").val(
-						document.location.protocol + "//"
-								+ document.location.hostname
-								+ "/app/config/read");
+				$("#urlvalue").val("/app/config/read");
 			} else if (document.location.hostname.slice(-11) == "opencpu.org") {
-				$("#urlvalue").val(
-						document.location.protocol + "//"
-								+ document.location.hostname + "/R/pub");
-			} else {
-				$("#urlvalue").val(document.location.href);
-			}
+				$("#urlvalue").val("/ocpu");
+			} 
 			populateCookieTable();
 			updateCookieWarning();
 
